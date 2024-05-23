b8e07e919cbb04ca41263aa7e8c8e55f52de4348
Ryota Asai <ryota846538@outlook.jp>
2024-05-23 20:33:07 +0900

    :construction: routing

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
+import Menu from '../pages/Menu';
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" />
       <Route path="/home" />
-      <Route path="/menu" />
+      <Route path="/menu" element={<Menu />} />
       <Route path="/mypage" />
     </Routes>
   );
