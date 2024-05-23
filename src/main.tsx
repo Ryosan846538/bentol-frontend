32817f83c6fd163be859f44bd91532907024ce61
Ryota Asai <ryota846538@outlook.jp>
2024-05-23 14:34:12 +0900

    :construction: Yamada-UIの追加とreact-router-domの初期設定

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { UIProvider } from "@yamada-ui/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UIProvider>
+        <App />
+      </UIProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
