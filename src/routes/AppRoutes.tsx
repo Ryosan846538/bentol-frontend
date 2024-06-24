b8a5d146246ad50830654c69acc2ce500ecbf97b
Ryota Asai <ryota846538@outlook.jp>
2024-06-24 17:35:15 +0900

    :gear: github-pages周りの微修正

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,17 +4,17 @@ import Login from '../pages/Login';
 import Menu from '../pages/Menu';
 import Home from '../pages/Home';
 import Booking from '../pages/Booking.tsx';
-import ShopHome from '../pages/ShopHome.tsx' ;
+import ShopHome from '../pages/ShopHome.tsx';
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />}/>
+      <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
       <Route path="/menu" element={<Menu />} />
       <Route path="/mypage" />
       <Route path="/booking" element={<Booking />} />
-      <Route path="/shophome" element={<ShopHome />}/>
+      <Route path="/shophome" element={<ShopHome />} />
     </Routes>
   );
 };
