80d97e54f9e6d472f949acaa14e078da77640898
Ryota Asai <ryota846538@outlook.jp>
2024-06-30 19:10:45 +0900

    :bug: 未使用のimportの削除

--- src/components/MyPageCard.tsx
+++ src/components/MyPageCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import OrderInfo from '../components/OrderInfo.tsx';
 import {
   Card,
@@ -9,66 +8,51 @@ import {
   Tabs,
   Tab,
   TabPanel,
-  Text,
-  VStack
+  VStack,
 } from '@yamada-ui/react';
 
-interface MyPageCardProps{
-    imageUrl: string;
-    title: string;
-    waiting: string;
-    num: string;
-    time: string;
+interface MyPageCardProps {
+  imageUrl: string;
+  title: string;
+  waiting: string;
+  num: string;
+  time: string;
 }
 
-const MyPageCard:React.FC<MyPageCardProps> = ({ imageUrl,title,waiting,num,time }) => {
+const MyPageCard: React.FC<MyPageCardProps> = ({ imageUrl, title, waiting, num, time }) => {
   return (
     <div>
-    <Card minW="sm" maxW="md" h="full" variant="solid" colorScheme="emerald">
-      <div className="p-2">
-      <Tabs variant="rounded-subtle" colorScheme="gray">
-        <Tab>品物</Tab>
-        <Tab>注文情報</Tab>
+      <Card minW="sm" maxW="md" h="full" variant="solid" colorScheme="emerald">
+        <div className="p-2">
+          <Tabs variant="rounded-subtle" colorScheme="gray">
+            <Tab>品物</Tab>
+            <Tab>注文情報</Tab>
 
-        <TabPanel>
-          <CardHeader justifyContent="center">
-            <Image 
-              src={imageUrl}
-              w="full"
-              rounded="md"
-            />
-          </CardHeader>
+            <TabPanel>
+              <CardHeader justifyContent="center">
+                <Image src={imageUrl} w="full" rounded="md" />
+              </CardHeader>
 
-          <CardBody>
-            <Heading
-             size="md"
-             color="white"
-            >{title}</Heading>
-          </CardBody>
-        </TabPanel>
-        <TabPanel>
-          <CardBody>
-            <VStack>
-              <OrderInfo 
-                title="お客様番号"
-                description={waiting}
-              />
-              <OrderInfo
-                title="予約時間"
-                description={time}
-              />
-              <OrderInfo
-                title="予約個数"
-                description={num}
-              />
-            </VStack>
-          </CardBody>
-        </TabPanel>
-      </Tabs>
-      </div>
-    </Card>      
+              <CardBody>
+                <Heading size="md" color="white">
+                  {title}
+                </Heading>
+              </CardBody>
+            </TabPanel>
+            <TabPanel>
+              <CardBody>
+                <VStack>
+                  <OrderInfo title="お客様番号" description={waiting} />
+                  <OrderInfo title="予約時間" description={time} />
+                  <OrderInfo title="予約個数" description={num} />
+                </VStack>
+              </CardBody>
+            </TabPanel>
+          </Tabs>
+        </div>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default MyPageCard
+export default MyPageCard;
